3de2267a07d6403afadb3a092035d4a9da42f304
huziao <1183623648@qq.com>
2021-01-22 15:47:16 +0800

    second

--- commnity/src/main/java/com/chinasoft/controller/AuthorizeController.java
+++ commnity/src/main/java/com/chinasoft/controller/AuthorizeController.java
@@ -1,6 +1,12 @@
 package com.chinasoft.controller;
 
+import com.chinasoft.dto.AccessTokenDto;
+import com.chinasoft.dto.GithubUser;
+import com.chinasoft.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /*
 接收github登录
@@ -8,5 +14,24 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class AuthorizeController {
 
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state
+                           ){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDto.setState(state);
+        accessTokenDto.setClient_id("732981b010f1832ffece");
+        accessTokenDto.setClient_secret("008a094a76e916b4d0d9728a259231b770bd76c2");
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName()   );
+
+        return "index";
+    }
 
 }
---
+++ commnity/src/main/java/com/chinasoft/provider/GithubProvider.java
@@ -0,0 +1,61 @@
+package com.chinasoft.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.chinasoft.dto.AccessTokenDto;
+import com.chinasoft.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class GithubProvider {
+
+    /**
+     * 获得所需要的access_token
+     */
+    public String getAccessToken(AccessTokenDto accessTokenDto){
+         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDto));
+            Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                String string = response.body().string();
+                String token = string.split("&")[0].split("=")[1];
+                System.out.println(token);
+                return token;
+        } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+    /**
+     * 获取返回的用户信息
+     * @param accessToken
+     * @return
+     */
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+
+            return githubUser;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+}
